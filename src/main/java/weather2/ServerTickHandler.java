d79677ab3eb205a528a237acf34489505adf4492
Corosus <cordonfreeman@gmail.com>
2015-01-03 15:00:39 -0500

    removed IMC spam

--- src/main/java/weather2/ServerTickHandler.java
+++ src/main/java/weather2/ServerTickHandler.java
@@ -86,7 +86,7 @@ public class ServerTickHandler
 	    	listMsgs = FMLInterModComms.fetchRuntimeMessages(Weather.modID);
 	    	for (int i = 0; i < listMsgs.size(); i++) {
 	    		
-	    		System.out.println("Weather2 side: " + listMsgs.get(i).key + " - modID: " + listMsgs.get(i).getSender() + " - source: " + listMsgs.get(i).toString() + " - " + listMsgs.get(i).getNBTValue());
+	    		//System.out.println("Weather2 side: " + listMsgs.get(i).key + " - modID: " + listMsgs.get(i).getSender() + " - source: " + listMsgs.get(i).toString() + " - " + listMsgs.get(i).getNBTValue());
 	    		
 	    		if (listMsgs.get(i).key.equals("weather.raining")) {
 
