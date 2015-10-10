4140500d6fbe76ac0f210d38c5d8c176a22fe9c1
Corosauce <cordonfreeman@gmail.com>
2015-10-10 13:18:48 -0400

    version bump

--- src/main/java/weather2/Weather.java
+++ src/main/java/weather2/Weather.java
@@ -26,7 +26,7 @@ import cpw.mods.fml.common.network.FMLEventChannel;
 import cpw.mods.fml.common.network.NetworkRegistry;
 
 //@NetworkMod(channels = { "WeatherData", "EZGuiData" }, clientSideRequired = true, serverSideRequired = true, packetHandler = WeatherPacketHandler.class)
-@Mod(modid = "weather2", name="weather2", version="v2.3.9")
+@Mod(modid = "weather2", name="weather2", version="v2.3.10")
 public class Weather {
 	
 	@Mod.Instance( value = "weather2" )
