cce9746ad25d7d4cb37706d8795af7f6ca26665b
CosmicDan <dconnolly.au@gmail.com>
2017-01-14 22:38:01 +1100

    WeatherManagerServer optimisations [...]  - Use counters instead of modulo's (much more CPU friendly)  - Allow these counters to be user-configured

--- src/main/java/weather2/weathersystem/WeatherManagerServer.java
+++ src/main/java/weather2/weathersystem/WeatherManagerServer.java
@@ -26,6 +26,14 @@ public class WeatherManagerServer extends WeatherManagerBase {
 	//storm logic, syncing to client
 	
 	public int syncRange = 256;
+	
+	private int tickerSyncWeatherCheckVanilla = 0;
+	private int tickerSyncWeatherLowWind = 0;
+	private int tickerSyncWeatherHighWind = 0;
+	private int tickerSyncVolcanos = 0;
+	private int tickerSyncWindAndIMC = 0;
+	private int tickerSyncStormSpawnOrRemoveChecks = 0;
+	
 
 	public WeatherManagerServer(int parDim) {
 		super(parDim);
@@ -39,6 +47,13 @@ public class WeatherManagerServer extends WeatherManagerBase {
 	@Override
 	public void tick() {
 		super.tick();
+		
+		tickerSyncWeatherCheckVanilla++;
+		tickerSyncWeatherLowWind++;
+		tickerSyncWeatherHighWind++;
+		tickerSyncVolcanos++;
+		tickerSyncWindAndIMC++;
+		tickerSyncStormSpawnOrRemoveChecks++;
 		
 		World world = getWorld();
 		
@@ -61,50 +76,61 @@ public class WeatherManagerServer extends WeatherManagerBase {
 			}
 			
 			//if (ConfigMisc.overcastMode) {
-				if (world.getTotalWorldTime() % 400 == 0) {
+				if (tickerSyncWeatherCheckVanilla == ConfigMisc.tickerRateSyncWeatherCheckVanilla) {
 					isVanillaRainActiveOnServer = getWorld().isRaining();
 					syncWeatherVanilla();
+					tickerSyncWeatherCheckVanilla = 0;
+					//Weather.dbg("for dim: " + world.provider.dimensionId + " - is server dimension raining?: " + world.isRaining() + " time: " + world.getWorldInfo().getRainTime());
 				}
 			//}
-			
-			if (world.getTotalWorldTime() % 400 == 0) {
-				//Weather.dbg("for dim: " + world.provider.dimensionId + " - is server dimension raining?: " + world.isRaining() + " time: " + world.getWorldInfo().getRainTime());
-			}
 			
 			//sync storms
 			
 			//System.out.println("getStormObjects().size(): " + getStormObjects().size());
 			
-			for (int i = 0; i < getStormObjects().size(); i++) {
-				StormObject so = getStormObjects().get(i);
-				if (world.getTotalWorldTime() % ((so.levelCurIntensityStage >= StormObject.STATE_HIGHWIND) ? 2 : 40) == 0) {
-					syncStormUpdate(so);
+			boolean shouldUpdateHighWind = false;
+			boolean shouldUpdateLowWind = false;
+			if (tickerSyncWeatherHighWind == ConfigMisc.tickerRateSyncWeatherHighWind) {
+				shouldUpdateHighWind = true;
+				tickerSyncWeatherHighWind = 0;
+			}
+			if (tickerSyncWeatherLowWind == ConfigMisc.tickerRateSyncWeatherLowWind) {
+				shouldUpdateLowWind = true;
+				tickerSyncWeatherLowWind = 0;
+			}
+			
+			if (shouldUpdateHighWind || shouldUpdateLowWind) {
+				for (int i = 0; i < getStormObjects().size(); i++) {
+					StormObject so = getStormObjects().get(i);
+					if (so.levelCurIntensityStage >= StormObject.STATE_HIGHWIND) {
+						if (shouldUpdateHighWind)
+							syncStormUpdate(so);
+					} else if (shouldUpdateLowWind)
+						syncStormUpdate(so);
 				}
 			}
 			
-			
 			//sync volcanos
-			if (world.getTotalWorldTime() % 40 == 0) {
+			if (tickerSyncVolcanos == ConfigMisc.tickerRateSyncVolcanos) {
+				tickerSyncVolcanos = 0;
 				for (int i = 0; i < getVolcanoObjects().size(); i++) {
 					syncVolcanoUpdate(getVolcanoObjects().get(i));
 				}
 			}
 			
-			//sync wind
-			if (world.getTotalWorldTime() % 60 == 0) {
+			//sync wind and IMC
+			if (tickerSyncWindAndIMC == ConfigMisc.tickerRateSyncWindAndIMC) {
 				syncWindUpdate(windMan);
-			}
-			
-			//IMC
-			if (world.getTotalWorldTime() % 60 == 0) {
 				nbtStormsForIMC();
+				tickerSyncWindAndIMC = 0;
 			}
 			
 			//temp
 			//getVolcanoObjects().clear();
 			
 			//sim box work
-			if (WeatherUtilConfig.listDimensionsClouds.contains(world.provider.dimensionId) && world.getTotalWorldTime() % 20 == 0) {
+			if (WeatherUtilConfig.listDimensionsClouds.contains(world.provider.dimensionId) && (tickerSyncStormSpawnOrRemoveChecks == ConfigMisc.tickerRateSyncStormSpawnOrRemoveChecks)) {
+				tickerSyncStormSpawnOrRemoveChecks = 0;
 				for (int i = 0; i < getStormObjects().size(); i++) {
 					StormObject so = getStormObjects().get(i);
 					EntityPlayer closestPlayer = world.getClosestPlayer(so.posGround.xCoord, so.posGround.yCoord, so.posGround.zCoord, ConfigMisc.Misc_simBoxRadiusCutoff);
